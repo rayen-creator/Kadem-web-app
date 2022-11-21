16e82af3f6ee923e5d3c4f5c78d1a7580b8ca277
Rayen Oueslati <rayenoueslati60@gmail.com>
2022-11-21 12:17:03 +0100

    updated backoffice routing

--- src/app/components/back-office/back-office-routing.module.ts
+++ src/app/components/back-office/back-office-routing.module.ts
@@ -2,7 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BackOfficeComponent } from './back-office.component';
 
-const routes: Routes = [{ path: '', component: BackOfficeComponent }];
+const routes: Routes = [
+  {
+    path: '', component: BackOfficeComponent, children: [
+      //
+
+    ]
+  }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
