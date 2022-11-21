04950d1c5175a136597debe4c5ec17e5b6b97bbe
Rayen Oueslati <rayenoueslati60@gmail.com>
2022-11-21 12:14:55 +0100

    updated header component

--- src/app/components/back-office/shared/header/header.component.ts
+++ src/app/components/back-office/shared/header/header.component.ts
@@ -1,19 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { AuthService } from 'src/app/core/helpers/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
-
-  constructor(private router:Router) { }
+export class HeaderComponent implements OnInit, OnDestroy {
+  username:string;
+  userIsAuthenticated = false;
+  private authListenerSubs: Subscription;
+  constructor(private router: Router,private auth:AuthService) { }
 
   ngOnInit(): void {
+    this.userIsAuthenticated = this.auth.isUserAuth();
+    this.authListenerSubs = this.auth.getAuthStatusListener().subscribe(
+      (isAuthenticated) => {
+        this.userIsAuthenticated = isAuthenticated;
+      }
+    )
+    this.username=this.auth.getUserEmail();
+    console.log("userIsAuthenticated : "+this.userIsAuthenticated);
   }
-  goTologin(){
-    this.router.navigate(['/login']);
 
-}
+  goTologin() {
+    this.router.navigate(['/login']);
+  }
+
+  ngOnDestroy(): void {
+    this.authListenerSubs.unsubscribe();
+ }
+ logout(){
+  this.auth.logout();
+ }
 }
