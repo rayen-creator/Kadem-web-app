f8911f270ab8e1060966080c3ed1985f73f9dcbd
Rayen Oueslati <rayenoueslati60@gmail.com>
2022-12-01 09:47:36 +0100

    ✨ adding toast package

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,10 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AuthInterceptorInterceptor } from './core/helpers/auth-interceptor.interceptor';
 import { AuthGuard } from './core/helpers/auth.guard';
 import { AuthService } from './core/helpers/auth.service';
+import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { ToastrModule } from 'ngx-toastr';
 
 
 @NgModule({
@@ -29,7 +33,15 @@ import { AuthService } from './core/helpers/auth.service';
     BrowserModule,
     ReactiveFormsModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule,
+    BrowserAnimationsModule, // required animations module
+    ToastrModule.forRoot({
+      timeOut: 3500,
+      positionClass: 'toast-bottom-right',
+      preventDuplicates: true,
+    }), // ToastrModule added
+
   ],
   providers: [AuthService,
     [{
